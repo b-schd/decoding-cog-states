25d7451c6fc73b6e72302ba04933a403a9226159
Brittany Scheid <bscheid@seas.upenn.edu>
2021-10-12 16:17:47 -0400

    add bandpass filtering to data loader

--- proj_config.m
+++ proj_config.m
@@ -2,8 +2,8 @@
 function p = proj_config()
 
 % Parameters for obtaining data
-p.ddir = '/Users/graceng/OneDrive/CCDTnewDat/CCDT/eeg/'; %'/Users/bscheid/Documents/LittLab/PROJECTS/p15_Cog_Performance/CCDT/eeg/'; % data directory
-p.subjChLoc = '/Users/graceng/Documents/Med_School/Research/BCI/glasso_data/patient_loc.mat';
+p.ddir = '/Users/bscheid/Documents/LittLab/PROJECTS/p15_Cog_Performance/CCDT/eeg/'; % '/Users/graceng/OneDrive/CCDTnewDat/CCDT/eeg/';
+p.subjChLoc = '/Users/bscheid/Documents/LittLab/PROJECTS/p15_Cog_Performance/CCDT/patient_loc.mat';
 p.subj = 'HUP069'; %'HUP142'; % string name of the subject (eg, 'HUP069') or 
 % [] to process all subjects
 p.stsubj = 1; %index of first subject if processing all subjects
@@ -16,6 +16,7 @@ p.reref = 'noreref';
 p.rln = 1; % remove line noise? (0 or 1)
 p.rrf = 1; % re-reference? (0 = none, 1 = common average re-reference, 2 = bipolar re-reference, 3 = laplacian re-reference)
             % always use 0 (no re-referencing for CCDT data)
+p.bpfilt = [.5, 150]; % band pass filter cuttoffs (Hz) (leave empty if no filter)
 p.outl = 1; % remove outlier channels? (0 or 1)
 p.outlMetric = 'powGamma'; % outlier metric ('rms' = root-median-squared, 'powGamma' = high-gamma power)
 p.outlThresh = 5; % outlier threshold (standard deviations)
@@ -30,6 +31,6 @@ p.winLength = 2500; % length of time window (ms) to analyze prior to event of in
 p.Lwin = 0.5; % length of window (s) to compute controllability
 p.glassoPath = './'; 
 p.gamma = .25;
-p.beta = 1;
+p.beta = [];
 
 end
